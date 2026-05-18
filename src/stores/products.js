349554c5ae7207645d0fdbb40a6e4a373b9decd7
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-18 21:42:17 +0100

    Change data source: from local data to public api data

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,85 +1,78 @@
-import { defineStore } from "pinia";
-import productsData from "../json/products.json";
+import { defineStore } from 'pinia'
 
-export const ProductsModule = defineStore("productsModule", {
+export const ProductsModule = defineStore('productsModule', {
   state: () => ({
-    flashDeals: productsData.products.slice(0, 8),
-    newProducts: productsData.products.slice(8, 16),
-    mobilePhones: productsData.products.slice(16, 24),
-    televisions: productsData.products.slice(24, 29),
-    groceries: productsData.products
-      .filter((el) => el.category === "groceries")
-      .slice(0, 8),
+    flashDeals: [],
+    newProducts: [],
+    mobilePhones: [],
+    televisions: [],
+    groceries: [],
     categories: [
       {
-        title: "Smart Phones",
-        route: "smartphones",
+        title: 'Smart Phones',
+        route: 'smartphones',
       },
       {
-        title: "Laptops",
-        route: "laptops",
+        title: 'Laptops',
+        route: 'laptops',
       },
       {
-        title: "Furniture",
-        route: "furniture",
+        title: 'Furniture',
+        route: 'furniture',
       },
       {
-        title: "Televisions",
-        route: "televisions",
+        title: 'Televisions',
+        route: 'televisions',
       },
       {
         title: "Men's Watches",
-        route: "mens-watches",
+        route: 'mens-watches',
       },
       {
         title: "Women's Bags",
-        route: "womens-bags",
+        route: 'womens-bags',
       },
       {
-        title: "Home Appliances",
-        route: "home-appliances",
+        title: 'Home Appliances',
+        route: 'home-appliances',
       },
     ],
     categoryProducts: [],
-    singleProduct: "",
+    singleProduct: '',
   }),
   actions: {
-    getProducts() {
-      //   try {
-      //     let res = await fetch("https://dummyjson.com/products");
-      //     let data = await res.json();
-      //     this.FlashDeals = data.products.slice(0, 8);
-      //     console.log(this.flashDeals);
-      //   } catch (err) {
-      //     console.log(err);
-      //   }
-      //   this.FlashDeals = productsData.products.slice(0, 8);
-      //   console.log(this.flashDeals);
-      // setTimeout(() => {
-      //   this.flashDeals = productsData.products.slice(0, 8);
-      //   this.newProducts = productsData.products.slice(8, 16);
-      // }, 3000);
+    async getProducts() {
+      try {
+        let res = await fetch('https://dummyjson.com/products')
+        let data = await res.json()
+        this.FlashDeals = data.products.slice(0, 8)
+        this.newProducts = data.products.slice(8, 16)
+        this.mobilePhones = data.products.slice(16, 24)
+        this.televisions = data.products.slice(24, 29)
+        this.groceries = data.products.filter((el) => el.category === 'groceries').slice(0, 8)
+      } catch (err) {
+        console.log(err)
+      }
     },
     async getProductsByCategory(category) {
       try {
-        let url = `https://dummyjson.com/products/category/${category}`;
-        let res = await fetch(url);
-        let data = await res.json();
-        this.categoryProducts = data.products;
+        let url = `https://dummyjson.com/products/category/${category}`
+        let res = await fetch(url)
+        let data = await res.json()
+        this.categoryProducts = data.products
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     },
     async getSingleProduct(productId) {
       try {
-        let url = `https://dummyjson.com/products/${productId}`;
-        let res = await fetch(url);
-        let data = await res.json();
-        this.singleProduct = data;
-        console.log(data);
+        let url = `https://dummyjson.com/products/${productId}`
+        let res = await fetch(url)
+        let data = await res.json()
+        this.singleProduct = data
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     },
   },
-});
+})
